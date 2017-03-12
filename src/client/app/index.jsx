16b536c33c4e1d4ff9209acc94c7e7f6c1e16de9
daveyx <d.rehle@gmail.com>
2017-03-12 02:49:00 +0100

    added weatherapp logic

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import {render} from 'react-dom';
+import axios from 'axios';
 
-class App extends React.Component {
-  render () {
-    return <p>Hello React!</p>;
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "city not set",
+      country: "country not set",
+      temp: "temp not set"
+    };
   }
-}
+  componentDidMount() {
+    this.clientInfo()
+  }
+  weatherInfo() {
+    axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.state.city + "," + this.state.country + "&units=metric&APPID=3c95b8b52fbaa600c02df827c3ca246c")
+      .then(response => {
+      this.setState({
+        temp: response.data.main.temp
+      });
+    }).catch(function (error) {
+      console.log("error axios-get2: " + error);
+    });
+  }
+  clientInfo() {
+    axios.get("http://ipinfo.io")
+      .then(response => {
+      this.setState({
+        city: response.data.city,
+        country: response.data.country
+      });
+      this.weatherInfo();
+    }).catch(function (error) {
+      console.log("error axios-get1: " + error);
+    });
+  }
+  render() {
+    var fah = Math.round( (Number(this.state.temp) * 9)/5 + 32 );
+    return <div class="col-xs-4 col-xs-offset-4">{this.state.city}, {this.state.country}, {this.state.temp}&#176;C / {fah} &#176;F</div>
+  }
+};
 
-render(<App/>, document.getElementById('app'));
+render(<Application />, document.getElementById('container'));
